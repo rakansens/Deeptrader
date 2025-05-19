6e7b421c8dca652daa7966aa59c4145df1502231
rakansens <rakansens@gmail.com>
2025-05-19 18:03:43 +0700

    chore: remove unused chart component

--- src/types/chart.ts
+++ src/types/chart.ts
@@ -23,5 +23,4 @@ export type IndicatorsChangeHandler = (
   value: IndicatorOptions,
 ) => void;
 
-export type { ChartConfig } from '../components/ui/chart';
 export type { ChartTheme } from '../hooks/use-chart-theme';
